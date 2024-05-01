cd7470b14a85400b5ac5e62f48a23b409ddb6e05
seowon <bsw0825@naver.com>
2024-05-01 09:31:56 +0900

    demo 프로젝트 기초세팅 완료

---
+++ demo/src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
+public class MyController {
+	
+	@RequestMapping("/")
+	public String root() {
+		log.info("root....");
+		return "index";
+	}
+}
